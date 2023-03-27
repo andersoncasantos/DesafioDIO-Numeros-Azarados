2db037a9f739e750f9e08ced591185923bb6bf09
Anderson <andersonmtv07@gmail.com>
2023-03-27 03:04:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+
+{
+    public static void Main()
+
+    {
+        string numero = Console.ReadLine();
+        Console.WriteLine(numero.Contains("13") ? $"{numero} es de Mala Suerte" : $"{numero} NO es de Mala Suerte");
+    }
+}
+
+
